cf4f9b7c6bb4a67bf75414dad018f470ee389d84
Nocchia <133043574+NomakCooper@users.noreply.github.com>
2025-04-01 05:26:25 +0000

    fix charts docs

--- plugins/modules/charts.py
+++ plugins/modules/charts.py
@@ -14,7 +14,7 @@ module: charts
 short_description: Generate high-quality charts using Plotly and save them as images.
 description:
   - This module generates various types of charts like C(line), C(bar), C(pie) and C(donut) and saves them as images.
-  - It provides customization options for O(titles), axis labels, colors, sizes, fonts, and legends.
+  - It provides customization options for titles, axis labels, colors, sizes, fonts, and legends.
   - Uses Plotly and Kaleido for image generation.
 version_added: "0.1.0"
 requirements:
