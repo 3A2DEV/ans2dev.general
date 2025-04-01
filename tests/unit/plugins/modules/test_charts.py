1272ceb9cf12e390040552e1b77bd696222d8f5a
Nocchia <133043574+NomakCooper@users.noreply.github.com>
2025-04-01 04:43:51 +0000

    try integration errors

--- tests/unit/plugins/modules/test_charts.py
+++ tests/unit/plugins/modules/test_charts.py
@@ -65,7 +65,7 @@ class TestChartsModule(unittest.TestCase):
             result_str = str(context.exception)
             # Check that exit_json was called and that the result indicates success.
             self.assertIn("exit_json called", result_str)
-            self.assertIn("'changed': true", result_str)
+            self.assertIn("'changed': True", result_str)
 
 
 if __name__ == '__main__':
